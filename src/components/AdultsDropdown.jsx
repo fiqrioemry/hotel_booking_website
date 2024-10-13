ab30aaafcd8d183f55f42d795b2a2b0f47b78a4b
fiqrioemry <foemry@gmail.com>
2024-10-13 23:43:29 +0700

    adding booking function

--- src/components/AdultsDropdown.jsx
+++ src/components/AdultsDropdown.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RoomContext } from "../context/RoomContext";
+import { Menu } from "@headlessui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const lis = [
+  { name: "1 Adults" },
+  { name: "2 Adults" },
+  { name: "3 Adults" },
+  { name: "4 Adults" },
+];
 
 const AdultsDropdown = () => {
-  return <div>AdultsDropdown</div>;
+  const {} = useContext(RoomContext);
+  return (
+    <Menu as="div">
+      <Menu.Button>
+        Adults <BsChevronDown className="" />
+      </Menu.Button>
+      <Menu.Items as="ul"></Menu.Items>
+    </Menu>
+  );
 };
 
 export default AdultsDropdown;
